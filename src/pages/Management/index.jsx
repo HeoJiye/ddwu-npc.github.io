c1b74f64fa2a02f4259a0333b69e660e45ec15e2
HeoJiye <heoze1031@gmail.com>
2023-05-06 20:53:12 +0900

    모집일정, 운영진 명단, Q&A 업로드

--- src/pages/Management/index.jsx
+++ src/pages/Management/index.jsx
@@ -1,8 +1,45 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Management extends Component {
+import styles from "./style.module.scss";
+
+const Member = (props) => {
+  return (
+    <div>
+      <img></img>
+      <div>
+        <div className={styles.member_name}>{props.name}</div>
+        <div className={styles.member_position}>{props.position}</div>
+      </div>
+    </div>
+  );
+};
+
+class Management extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      member: [
+        { name: "김예지", position: "동장" },
+        { name: "장영선", position: "기획팀장" },
+        { name: "윤수연", position: "개발팀장" },
+        { name: "권유진", position: "디자인팀장" },
+        { name: "황지민", position: "홍보팀장" },
+        { name: "홍성주", position: "DB팀장" },
+      ],
+    };
+  }
   render() {
-    return <div></div>;
+    return (
+      <div className={styles.page}>
+        <h2>운영진 명단</h2>
+        <div>
+          {this.state.member.map((e) => (
+            <Member name={e.name} position={e.position}></Member>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
--- src/pages/Schedule/index.jsx
+++ src/pages/Schedule/index.jsx
@@ -1,8 +1,98 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Schedule extends Component {
+import "./Schedule.css";
+
+const Detail = (props) => {
+  return (
+    <>
+      <div className="detail">
+        <div className="box">
+          <div className="detail_name">{props.name}</div>
+          <div className="detail_period">{props.period}</div>
+        </div>
+      </div>
+      <Next is_last={props.is_last}></Next>
+    </>
+  );
+};
+
+const Next = (props) => {
+  return (
+    <div>
+      {props.is_last === false ? (
+        <div className="circle">
+          <div className="triangle" />
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+const Qualifiacation = (props) => {
+  return (
+    <div className="team">
+      <div className="teamname">{props.teamname}</div>
+      <div className="teaminfo">{props.text}</div>
+    </div>
+  );
+};
+
+class Schedule extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      detail: [
+        { name: "모집기간", period: "5/7 ~ 5/14", is_last: false },
+        { name: "1차 합격 발표", period: "5/14", is_last: false },
+        { name: "면접기간", period: "5/7 ~ 5/14", is_last: false },
+        { name: "최종 합격자 발표", period: "5/14", is_last: true },
+      ],
+      teaminfo: [
+        {
+          teamname: "기획팀",
+          text: "텍스트 들어갈 자리 텍스트 들어갈 자리 텍스트 들어갈 자리 텍스트 들어갈 자리텍스트 들어갈 자리",
+        },
+        {
+          teamname: "디자인팀",
+          text: "텍스트 들어갈 자리 텍스트 들어갈 자리 텍스트 들어갈 자리 텍스트 들어갈 자리텍스트 들어갈 자리",
+        },
+        {
+          teamname: "개발팀",
+          text: "텍스트 들어갈 자리 텍스트 들어갈 자리 텍스트 들어갈 자리 텍스트 들어갈 자리텍스트 들어갈 자리 텍스트 들어갈 자리 텍스트 들어갈 자리",
+        },
+      ],
+    };
+  }
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div id="schedule">
+          <h2>모집일정</h2>
+          <div className="details">
+            {this.state.detail.map((e) => (
+              <Detail
+                name={e.name}
+                period={e.period}
+                is_last={e.is_last}
+              ></Detail>
+            ))}
+          </div>
+        </div>
+        <hr />
+        <div id="qualifiacation">
+          <h2>지원 자격</h2>
+          <div className="teamlist">
+            {this.state.teaminfo.map((e) => (
+              <Qualifiacation
+                teamname={e.teamname}
+                text={e.text}
+              ></Qualifiacation>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
